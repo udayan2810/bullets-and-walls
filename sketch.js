6f58229e38f67aae8ab60f86d4c7c9a769612afa
udayan2810 <64144265+udayan2810@users.noreply.github.com>
2020-05-01 20:29:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var bullet,wall;
+var speed,weight;
+function setup() {
+  createCanvas(1600,400);
+  speed=random(55,90);
+  weight=random(400,1500);
+  bullet = createSprite(50, 200, 50, 50);
+  wall = createSprite(1450,200,60,height/2);
+ wall.shapeColor="white";
+  bullet.velocityX=speed;
+}
+
+function draw() {
+  background(0,0,0);
+  if(wall.x-bullet.x < (bullet.width+wall.width)/2)
+  {bullet.velocityX=0;
+    var deformation=0.5 * weight * speed * speed/22500;
+
+    if (deformation > 180){
+bullet.shapeColor=color(255,0,0);
+    }
+    if(deformation > 180 && deformation>100){
+bullet.shapeColor=color(230,230,0);
+    }
+      if(deformation<100){
+bullet.shapeColor=color(0,2550,0);
+      }
+  }
+   drawSprites();
+}
